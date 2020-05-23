505b5abd990ccbbde4d6a22460f5f4c6a70e8011
Joseph <josephagrane@gmail.com>
2020-05-23 15:46:56 +0100

    removed uneccessary files

--- Source/TestSource3.py
+++
@@ -1,8 +0,0 @@
-#@qdclass(An object that represents abdlehak)
-class abdelhak:
-	def __init__(self, name, age):
-		#@qdmfield(The man's name*string)
-		self.name = name
-		#@qdmfield(The man's age*int)
-		self.age = age
-#@qdend
--- Source/TestSource4.py
+++
@@ -1,8 +0,0 @@
-#@qdclass(Abdelhak!)
-class abdelhak:
-    def __init__(self, name, age):
-        #@qdmfield(Their name*string)
-        self.name = name
-        #@qdmfield(Their age*int)
-        self.age = age
-#@qdend
